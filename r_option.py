6f504bc1917cda55c09c1b17d6039f91822c96ea
xamidovscode <xamidovcoder@gmail.com>
2025-01-22 19:20:48 +0500

    news

---
+++ r_option.py
@@ -0,0 +1,9 @@
+
+
+with open("example.txt", "r") as file:
+    line = file.readline()
+    while line:
+        print(line.rstrip())
+        line = file.readline()
+#Malumotlarni qatorma qator o’qish
+
