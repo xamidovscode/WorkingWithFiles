805e92e0066cf2ff59cc015e50ba306cc642e6a1
xamidovscode <xamidovcoder@gmail.com>
2025-01-22 19:58:48 +0500

    news

--- os_module.py
+++ os_module.py
@@ -1,9 +1,20 @@
+# import os
+#
+# # Faylni o'chirish
+# file_path = "/home/joxacode/Documents/x.pptx"
+# if os.path.exists(file_path):
+#     os.remove(file_path)
+#     print(f"Fayl {file_path} o'chirildi.")
+# else:
+#     print("Fayl mavjud emas!")
+
+
 import os
 
-# Faylni o'chirish
-file_path = "/home/joxacode/Documents/x.pptx"
-if os.path.exists(file_path):
-    os.remove(file_path)
-    print(f"Fayl {file_path} o'chirildi.")
+dir_path = "/home/joxacode/Documents/WorkingWithFiles"
+
+if os.path.exists(dir_path) and os.path.isdir(dir_path):
+    os.rmdir(dir_path)  # Bo'sh direktoriyani o'chiradi
+    print(f"Direktoriya {dir_path} o'chirildi.")
 else:
-    print("Fayl mavjud emas!")
+    print("Direktoriyaning mavjud emasligi yoki u bo'sh emasligi.")
