31eea71cba92524476af322689a73b3b6f5e1fac
xamidovscode <xamidovcoder@gmail.com>
2025-01-22 19:28:39 +0500

    news

--- w_option.py
+++ w_option.py
@@ -1,5 +1,12 @@
-#file ga yozish
-with open("example.txt", "w") as file:
-    file.write("Bu yangi fayl mazmuni.\n")
-    file.write("Ikkinchi qator.\n")
+# #file ga yozish
+# with open("example.txt", "w") as file:
+#     file.write("Bu yangi fayl mazmuni.\n")
+#     file.write("Ikkinchi qator.\n")
+
+
+with open("example.txt", "a") as file:
+    file.write("Bu yangi qo'shilgan 1-qator.\n")
+    file.write("Bu yangi qo'shilgan 2-qator.\n")
+
+
 
